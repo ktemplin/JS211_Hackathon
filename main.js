a44eee36e1bbc5c7f7e81b7e793012fd3921a1f3
Kris Templin <kris.templin@gmail.com>
2023-11-27 19:48:56 -0800

    Mostly functional, but good enough for now. Need to fix the visuals for the HTML rendering.

--- main.js
+++ main.js
@@ -1,18 +1,29 @@
-// const inputField = NULL
+let hangmanWord = ''
+let guessedLetters = new Set();
+let incorrectGuesses = 0;
+
+const maxIncorrectGuesses = 7;
+
+const wordDisplayElement = document.getElementById('wordDisplay');
+const feedbackElement = document.getElementById('feedback');
+const guessInputElement = document.getElementById('guessInput');
+const hangmanStageElement = document.getElementById('hangmanStage');
+
 
 const wordChoice = async () => {
     try{
     const response = await fetch('https://api.api-ninjas.com/v1/randomword/', {
         method: "GET", 
         headers: {
-            'X-Api-Key': '<API-KEY>'},
+            'X-Api-Key': 'YibgIotsYasSGOUR6SFagi7NTuTCdx9P0zQD37dT'},
         contentType: 'application/json'
             });
             if(response.ok){
                 const jsonResponse = await response.json()
                 // console.log(jsonResponse)
-                const word = jsonResponse.word.toLowerCase();
-                console.log(word);          
+                hangmanWord = jsonResponse.word.toLowerCase();
+                console.log(hangmanWord);
+                displayWord()         
             }
           }
           catch(error){
@@ -20,18 +31,80 @@ const wordChoice = async () => {
           }
         }
 
-const hangWord = wordChoice()
+const displayWord = () => {
+    const wordDisplay = hangmanWord
+        .split('')
+        .map(letter => (guessedLetters.has(letter) ? letter : '_'))
+        .join(' ');
+    wordDisplayElement.textContent = wordDisplay;
 
+    checkWin()
+    }
 
-// const request = require('request');
+    const makeGuess = () => {
+        const guess = guessInputElement.value.toLowerCase();
+      
+        if (guess.length !== 1 || !/[a-z]/.test(guess)) {
+          feedbackElement.textContent = 'Please enter a valid single letter.';
+          return;
+        }
+      
+        if (guessedLetters.has(guess)) {
+          feedbackElement.textContent = 'You already guessed that letter. Try again.';
+          return;
+        }
+      
+        guessedLetters.add(guess);
+      
+        if (hangmanWord.includes(guess)) {
+          feedbackElement.textContent = 'Good guess!';
+        } else {
+          feedbackElement.textContent = 'Incorrect guess. Try again.';
+          incorrectGuesses++;
+          updateHangmanStage();
+        }
+      
+        displayWord();
+        guessInputElement.value = '';
+      
+        checkLose();
+      }
 
-// request.get({
-//   url: 'https://api.api-ninjas.com/v1/randomword',
-//   headers: {
-//     'X-Api-Key': '2pg9V7TvcUpST/ygRiH9kw==YcfrBWjyrbT1t4ay'
-//   },
-// }, function(error, response, body) {
-//   if(error) return console.error('Request failed:', error);
-//   else if(response.statusCode != 200) return console.error('Error:', response.statusCode, body.toString('utf8'));
-//   else console.log(body)
-// });
+      const generateHangmanStage = () => {
+        const hangmanParts = ['rope', 'head', 'body', 'left-arm', 'right-arm', 'left-leg', 'right-leg'];
+      
+        hangmanParts.forEach(part => {
+          const partElement = document.createElement('div');
+          partElement.classList.add('hangman-part', part);
+          hangmanStageElement.appendChild(partElement);
+        });
+      }
+      
+      const updateHangmanStage = () => {
+        const hangmanParts = document.querySelectorAll('.hangman-part');
+      
+        if (incorrectGuesses <= maxIncorrectGuesses) {
+          hangmanParts[incorrectGuesses - 1]?.classList.add('visible');
+        }
+      }
+      
+      const checkWin = () => {
+        if (!wordDisplayElement.textContent.includes('_')) {
+          feedbackElement.textContent = 'Congratulations! You won!';
+          disableInput();
+        }
+      }
+      
+      const checkLose = () => {
+        if (incorrectGuesses === maxIncorrectGuesses) {
+          feedbackElement.textContent = `Sorry, you lost. The correct word was "${hangmanWord}".`;
+          disableInput();
+        }
+      }
+      
+      const disableInput = () => {
+        guessInputElement.disabled = true;
+      }
+      
+      wordChoice()
+      generateHangmanStage()
